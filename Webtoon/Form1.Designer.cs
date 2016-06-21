2a6c2b94500bfa1932688a016a452257864fa12d
Cube <bedrook@gmail.com>
2016-06-21 12:38:32 +0900

    초기 커밋.

---
+++ Webtoon/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Webtoon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderSelect = new System.Windows.Forms.Button();
+            this.startPage = new System.Windows.Forms.TextBox();
+            this.endPage = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.folderPath = new System.Windows.Forms.TextBox();
+            this.URLTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderSelect
+            // 
+            this.folderSelect.Location = new System.Drawing.Point(717, 45);
+            this.folderSelect.Name = "folderSelect";
+            this.folderSelect.Size = new System.Drawing.Size(120, 23);
+            this.folderSelect.TabIndex = 0;
+            this.folderSelect.Text = "FolderSelect";
+            this.folderSelect.UseVisualStyleBackColor = true;
+            this.folderSelect.Click += new System.EventHandler(this.folderSelect_Click);
+            // 
+            // startPage
+            // 
+            this.startPage.Location = new System.Drawing.Point(683, 12);
+            this.startPage.Name = "startPage";
+            this.startPage.Size = new System.Drawing.Size(78, 25);
+            this.startPage.TabIndex = 2;
+            this.startPage.Text = "1";
+            // 
+            // endPage
+            // 
+            this.endPage.Location = new System.Drawing.Point(767, 12);
+            this.endPage.Name = "endPage";
+            this.endPage.Size = new System.Drawing.Size(70, 25);
+            this.endPage.TabIndex = 3;
+            this.endPage.Text = "10";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 387);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(824, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(761, 77);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(76, 47);
+            this.StartButton.TabIndex = 5;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 77);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(743, 300);
+            this.output.TabIndex = 6;
+            this.output.Text = " ";
+         
+            // 
+            // folderPath
+            // 
+            this.folderPath.Location = new System.Drawing.Point(12, 44);
+            this.folderPath.Name = "folderPath";
+            this.folderPath.Size = new System.Drawing.Size(699, 25);
+            this.folderPath.TabIndex = 7;
+            // 
+            // URLTextBox
+            // 
+            this.URLTextBox.Location = new System.Drawing.Point(13, 12);
+            this.URLTextBox.Name = "URLTextBox";
+            this.URLTextBox.Size = new System.Drawing.Size(664, 25);
+            this.URLTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(762, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 67);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Make HTML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 422);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.folderPath);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.endPage);
+            this.Controls.Add(this.startPage);
+            this.Controls.Add(this.URLTextBox);
+            this.Controls.Add(this.folderSelect);
+            this.Name = "Form1";
+            this.Text = "WebtoonDownloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button folderSelect;
+        private System.Windows.Forms.TextBox startPage;
+        private System.Windows.Forms.TextBox endPage;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.TextBox folderPath;
+        private System.Windows.Forms.TextBox URLTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Webtoon/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+
+namespace Webtoon
+{
+    public partial class Form1 : Form
+    {
+        LinkedList<string> outputstrs;
+        string tempImagePath;
+        string path;
+        string URL;
+        string outputstr;
+        int startNum;
+        int endNum;
+        int resultCount;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void init()
+        {
+            tempImagePath = "";
+            path = "";
+            URL = "";
+            startNum = 0;
+            endNum = 0;
+            resultCount = 0;
+            progressBar.Value = 0;
+            outputstrs = new LinkedList<string>();
+            
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (URLTextBox.Text.Equals(""))
+            {
+                MessageBox.Show("URL을 지정해주세요");
+                return;
+            }else if (folderPath.Text.Equals(""))
+            {
+                MessageBox.Show("폴더경로를 지정해주세요");
+                return;
+            }
+            
+            init();
+            URL = URLTextBox.Text;
+
+            try {
+                startNum = int.Parse(startPage.Text);
+                endNum = int.Parse(endPage.Text);
+                resultCount = startNum;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("잘못된 문자가 있습니다."+ex.ToString());
+                return;
+            }
+
+            progressBar.Maximum = endNum - startNum+1;
+            progressBar.Minimum = 0;
+
+            path = folderPath.Text + "\\";
+            tempImagePath =path+"temp";
+            if (!Directory.Exists(tempImagePath))
+                System.IO.Directory.CreateDirectory(tempImagePath);
+            tempImagePath += "\\";
+            tempFolderFilesRemove();
+
+            new Thread(new ThreadStart(mainloop)).Start();
+
+        }
+
+        /*폴더지정*/
+        private void folderSelect_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.ShowDialog();
+            folderPath.Text = FBD.SelectedPath;
+        }
+
+        /*파일 다운로드*/
+        /*URL 변형*/
+        private void URLEdit(ref string url, int num)
+        {
+            int startidx = url.IndexOf("&no=");
+            url = url.Remove(startidx);
+            url += "&no=" + num.ToString();
+
+        }
+        /*웹페이지의 request반환*/
+        private string getWebRequest(string webURL)
+        {
+            WebRequest request = WebRequest.Create(webURL);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            
+            return reader.ReadToEnd();
+        }
+        /*웹페이지에서 웹툰컷들 다운로드*/
+        private void pageDownload(string fullString, string start = "imgcomic.", string end = ".jpg")
+        {
+            int count = 0;
+            int count2 = 0;
+            int startidx = 0;
+            int endidx = 0;
+            String str = fullString;
+            WebClient WB = new WebClient();
+
+            char[] tempstr = new char[str.Length];
+
+            while (true)
+            {
+                startidx = str.IndexOf("imgcomic.");
+                do
+                {
+                    endidx = str.IndexOf(".jpg");
+
+                    if (startidx > endidx && endidx!=-1)
+                    {
+                        tempstr = str.ToArray<char>();
+                        tempstr[endidx] = ' ';
+                        str = new String(tempstr);
+                    }
+
+                } while (endidx < startidx && endidx != -1 && startidx != -1);
+
+
+                if (startidx != -1 && endidx != -1)
+                {
+                    string tempURL = str.Substring(startidx, endidx + 4 - startidx);
+                    count++;
+                    if (count == 10)
+                    {
+                        count2++;
+                        count -= 10;
+                    }
+                    try {
+                        WB.DownloadFile("http://" + tempURL, count2.ToString() + count.ToString() + ".jpg");
+                    }catch(Exception e)
+                    {
+                        MessageBox.Show("파일 다운로드중 오류 발생!"+e.Message);
+
+                    }
+                }
+                else
+                    break;
+
+                tempstr = str.ToArray<char>();
+                for (int i = startidx; i < endidx; i++)
+                    tempstr[i] = '!';
+                str = new String(tempstr);
+
+            }
+  
+            DirectoryInfo direcinfo = new DirectoryInfo(System.Environment.CurrentDirectory);
+            FileInfo[] fileinfo = direcinfo.GetFiles("*.jpg");
+            foreach(FileInfo f in fileinfo)
+                File.Move(f.FullName, tempImagePath + f.Name);
+        }
+        //////////////파일다운로드//////////
+
+        /*이미지 만들기*/
+        private void makeImage()
+        {
+            DirectoryInfo di = new DirectoryInfo(path + "temp");
+            FileInfo[] files = di.GetFiles("*.jpg");
+            List<Image> images = new List<Image>();
+            List<Bitmap> result = new List<Bitmap>();
+            List<Graphics> gra = new List<Graphics>();
+            const int cutmaxSize = 50000;
+            int heightSum = 0;
+            int widthSum = 0;
+
+            foreach (FileInfo f in files)
+            {
+                images.Add(Image.FromFile(f.FullName));
+                heightSum += images.LastOrDefault().Height;
+                
+            }
+            files = null;
+            di = null;
+            
+
+            if (images.Count == 0)
+                return;
+            widthSum = images[0].Width;
+
+            //이미지 사이즈 할당
+            
+            int tempHeightSum = heightSum;
+            Bitmap tempbmp;
+            while (cutmaxSize < tempHeightSum)
+            {
+                tempbmp = new Bitmap(widthSum, cutmaxSize);
+                result.Add(tempbmp);
+                gra.Add(Graphics.FromImage(tempbmp));
+                tempHeightSum -= cutmaxSize;
+               
+            }
+            tempbmp = new Bitmap(widthSum, tempHeightSum);
+            result.Add(tempbmp);
+            gra.Add(Graphics.FromImage(tempbmp));
+            tempbmp = null;
+          
+
+            int gount = 0;
+            tempHeightSum = 0;
+            for (int i= 0; i < images.Count; i++)
+            {
+                gra[gount].DrawImage(images[i], 0, tempHeightSum);
+                tempHeightSum += images[i].Height;
+                
+                if (tempHeightSum > cutmaxSize)
+                {
+                    tempHeightSum -= images[i].Height+cutmaxSize;
+                    gount++;
+                    i--;
+                }
+                
+            }
+            gount = 1;
+            foreach (Bitmap b in result)
+            {
+                b.Save(path + resultCount.ToString() + "_" + gount.ToString() + ".jpg", ImageFormat.Jpeg);
+                b.Dispose();
+                gount++;
+            }
+            foreach(Image img in images)
+            {
+                img.Dispose();
+            }
+            foreach(Graphics gr in gra)
+            {
+                gr.Dispose();
+            }
+
+            resultCount++;
+            result.Clear();
+            gra.Clear();
+            images.Clear();
+
+            GC.Collect();
+
+            
+        }
+        /*임시파일 삭제*/
+        private void tempFolderFilesRemove()
+        {
+            DirectoryInfo di = new DirectoryInfo(path + "temp");
+            FileInfo[] files = di.GetFiles("*.jpg");
+            
+            foreach (FileInfo f in files)
+                f.Delete();
+
+            di = null;
+
+        }
+
+        private void mainloop()
+        {
+            for (int pageNum = startNum; pageNum <= endNum; pageNum++)
+            {
+                
+                URLEdit(ref URL, pageNum);
+                pageDownload(getWebRequest(URL));
+                makeImage();
+                tempFolderFilesRemove();
+                progressBar.Value += 1;
+
+                if (outputstrs.Count < 20)
+                    outputstrs.AddLast(pageNum.ToString() + "\n");
+                else
+                {
+                    outputstrs.RemoveFirst();
+                    outputstrs.AddLast(pageNum.ToString() + "\n");
+                }
+                outputstr = "";
+                foreach(string s in outputstrs)
+                {
+                    outputstr += s; 
+                }
+                output.Text = outputstr;
+                
+            }
+            MessageBox.Show("끝!");
+
+        }
+
+        //웹페이지로 뷰어 만들기
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (folderPath.Text.Equals(""))
+                return;
+            path = folderPath.Text;
+            DirectoryInfo di = new DirectoryInfo(path);
+            for(int i = 1; di.GetFiles(i.ToString() + "_?.jpg").Count() != 0; i++)
+            {
+                FileInfo[] files = di.GetFiles(i.ToString() + "_*.jpg");
+                using (FileStream fs = new FileStream(path+"/" + i.ToString() + "Page.html", FileMode.OpenOrCreate))
+                {
+                    StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+                    sw.Write("<html><head><title>"+i.ToString()+"화</title><style>img{width: 100%;height: auto;}</style></head><body bgcolor=#000000>");
+                    foreach(FileInfo fi in files)
+                    {
+                        sw.Write("<img src="+fi.Name+"><br>");
+                    }
+                    sw.Write(
+                        " <font size=7><a href=" + (i - 1).ToString() + "Page.html> <strong>이전화</strong> </a> &nbsp &nbsp <font color=#ffffff>"+i.ToString()+ "화 </font> &nbsp &nbsp"
+                        + "<a href=" + (i + 1).ToString() + "Page.html> <strong>다음화</strong> </a> </body> </html>"
+                        );
+                    sw.Flush();
+                }
+                
+
+
+            }
+            MessageBox.Show(path+"Active!");
+        
+       }
+        
+    }
+}
